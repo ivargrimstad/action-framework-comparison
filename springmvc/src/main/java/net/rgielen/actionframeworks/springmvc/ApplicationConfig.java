399d234a51760fa9df09143d229ca48ef966e1f0
Rene Gielen <rgielen@apache.org>
2015-11-09 17:57:22 +0100

    add webjars as resource

--- springmvc/src/main/java/net/rgielen/actionframeworks/springmvc/ApplicationConfig.java
+++ springmvc/src/main/java/net/rgielen/actionframeworks/springmvc/ApplicationConfig.java
@@ -28,6 +28,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.thymeleaf.spring4.SpringTemplateEngine;
 import org.thymeleaf.spring4.view.ThymeleafViewResolver;
@@ -40,6 +41,11 @@ import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
 @ComponentScan(basePackageClasses = {ApplicationConfig.class, ActorService.class})
 @EnableWebMvc
 public class ApplicationConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
 
     @Bean
     public ServletContextTemplateResolver templateResolver(){
