42f3e9d7808c70bc979335851961914c42a60af1
Rene Gielen <rgielen@apache.org>
2015-11-09 17:57:47 +0100

    add UTF-8 encoding filter

--- springmvc/src/main/java/net/rgielen/actionframeworks/springmvc/ApplicationInitializer.java
+++ springmvc/src/main/java/net/rgielen/actionframeworks/springmvc/ApplicationInitializer.java
@@ -27,11 +27,11 @@ package net.rgielen.actionframeworks.springmvc;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.DispatcherServlet;
 
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRegistration;
+import javax.servlet.*;
+import java.util.EnumSet;
 
 /**
  * @author Rene Gielen
@@ -43,6 +43,15 @@ public class ApplicationInitializer implements WebApplicationInitializer {
         AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
         context.register(ApplicationConfig.class);
         servletContext.addListener(new ContextLoaderListener(context));
+
+        CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+        characterEncodingFilter.setEncoding("UTF-8");
+        characterEncodingFilter.setForceEncoding(true);
+
+        EnumSet<DispatcherType> dispatcherTypes = EnumSet.of(DispatcherType.REQUEST, DispatcherType.FORWARD);
+
+        FilterRegistration.Dynamic characterEncoding = servletContext.addFilter("characterEncoding", characterEncodingFilter);
+        characterEncoding.addMappingForUrlPatterns(dispatcherTypes, false, "/*");
 
         ServletRegistration.Dynamic registration =
                 servletContext.addServlet("dispatcher", new DispatcherServlet(context));
